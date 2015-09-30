6712c088b4a82ba9b0d05b3544219bee57c49535
David Katz <15Dkatz@shcp.edu>
2015-09-30 00:48:26 -0700

    Finished layout. Finished Functionality.

--- manila-philippines/ViewController.swift
+++ manila-philippines/ViewController.swift
@@ -6,13 +6,24 @@
 //  Copyright © 2015 davidkatz. All rights reserved.
 //
 
+
+//Compass Icon by <div>Icons made by <a href="http://www.flaticon.com/authors/simpleicon" title="SimpleIcon">SimpleIcon</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a>             is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0">CC BY 3.0</a></div>
+
+
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var findOutMoreBtn: UIButton!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        findOutMoreBtn.layer.cornerRadius = 4.0
+        
     }
 
     override func didReceiveMemoryWarning() {
